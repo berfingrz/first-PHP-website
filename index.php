9791cddd09731b8d5d6aa6cd6910e75aeffe5915
Berfin Gürz <gurzberfin@gmail.com>
2021-04-10 10:54:41 +0300

    This is new index.php upload information about an automobile

--- index.php
+++ index.php
@@ -5,6 +5,24 @@
 <?php
 echo "Welcome to PHP";
 ?>
+  <form method="POST" action="submit.php">
+    <p><label for="licencePlate">Licence Plate:</label>
+        <input type="text" name="licencePlate" size="40" />
+    </p>
+    <p><label for="brand">Brand:</label>
+        <input type="text" name="brand" size="40" />
+    </p>
+    <p><label for="model">Model:</label>
+        <input type="text" name="model" size="40" />
+    </p>
+    <p><label for="modelYear">Model Year:</label>
+        <input type="date" name="modelYear" size="40" />
+    </p>
+    <p><label for="color">Color:</label>
+        <input type="text" name="color" size="40" />
+    </p>
+    <input type="submit" />
 
+</form>
 </body>
 </html>
